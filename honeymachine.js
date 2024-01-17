7665ae98dca0b4cbaf9bca24c3f3cb34289a9532
Sebastian Krüger <sk@honeymachine.io>
2024-01-17 15:00:44 +0100

    chore: update year ans slogan

--- honeymachine.js
+++ honeymachine.js
@@ -4,8 +4,8 @@ if (!footerEl) {
   throw '';
 }
 
-const year = 2023;
-const footer = (y) => `<span>${y} © <a href="https://honeymachine.io" class="home-link">HONEYMACHINE</a>. Created with 🌶.</span>`
+const year = 2024;
+const footer = (y) => `<span>${y} © <a href="https://honeymachine.io" class="home-link">HONEYMACHINE</a>. DIY.</span>`
 const signature = document.createElement('div')
 signature.innerHTML = footer(year).trim();
 footerEl.appendChild(signature.firstChild)
