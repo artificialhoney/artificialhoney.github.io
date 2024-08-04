57880b6f99710a66f3d09c2fd419112d375ecc4d
Sebastian Krüger <sk@honeymachine.io>
2024-08-04 09:29:18 +0200

    feat: new dashboard

--- honeymachine.js
+++ honeymachine.js
@@ -5,7 +5,7 @@ if (!footerEl) {
 }
 
 const year = 2024;
-const footer = (y) => `<span>${y} © <a href="https://honeymachine.io" class="home-link">HONEYMACHINE</a>. DIY.</span>`
+const footer = (y) => `<span>${y} © <a href="https://hi.honeymachine.io" class="home-link">HONEYMACHINE</a>. DIY.</span>`
 const signature = document.createElement('div')
 signature.innerHTML = footer(year).trim();
 footerEl.appendChild(signature.firstChild)
